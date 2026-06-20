03dba1dc7536d0eb283b5c63767fe727304b3338
analogwings <narayan@yahoo.com>
2026-06-20 15:28:27 -0700

    Prepare release content

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,8 +7,8 @@ providers.gradleProperty("codexBuildDir").orNull?.let { customBuildDir ->
     layout.buildDirectory.set(file(customBuildDir))
 }
 
-val appVersionCode = 105
-val appVersionName = "dev-1.05"
+val appVersionCode = 104
+val appVersionName = "dev-1.04"
 val appDisplayVersion = "v$appVersionName ($appVersionCode)"
 val appDebugDisplayVersion = "v$appVersionName-dev ($appVersionCode)"
 val isProBuild = providers.gradleProperty("batteryCurrentPro")
